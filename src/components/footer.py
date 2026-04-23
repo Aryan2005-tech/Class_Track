aad19c38746f61c866062617e7b399b9d03b8b70
Aryan2005-tech <you@example.com>
2026-04-24 00:59:24 +0530

    implementation of Home Screen

---
+++ src/components/footer.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def footer_home():
+
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
+            <p style="font-weight:bold; color:white !important; margin:0;">Created by Aryan</p>
+        </div>
+        """, unsafe_allow_html=True)
