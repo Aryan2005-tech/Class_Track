2f311b2bda7455658e19009a21a723d7822bf26c
Aryan2005-tech <you@example.com>
2026-04-29 12:07:45 +0530

    connecting databse

--- src/database/config.py
+++ src/database/config.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from supabase import create_client,Client
-supabse:Client=create_client(
+supabase:Client=create_client(
     st.secrets['SUPABASE_URL'],
     st.secrets['SUPABASE_KEY']
 )
