f7961cd04bf7f3e86787a31dbf455a3f450e55ba
Aryan2005-tech <you@example.com>
2026-05-02 04:41:01 +0530

    finishing_touches

--- src/components/header.py
+++ src/components/header.py
@@ -6,7 +6,7 @@ def header_home():
     st.markdown(f"""
         <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:30px; margin-top:30px;">
             <img src='{logo_url}' style='height:100px; filter: drop-shadow(0 4px 12px rgba(0,0,0,0.2));' />
-            <h1 style='text-align:center; color:white !important; text-shadow: 0 2px 15px rgba(0,0,0,0.2);'>SNAP<br>CLASS</h1>
+            <h1 style='text-align:center; color:white !important; text-shadow: 0 2px 15px rgba(0,0,0,0.2);'>CLASS<br>TRACK</h1>
         </div>
     """, unsafe_allow_html=True)
 
@@ -16,6 +16,6 @@ def header_dashboard():
     st.markdown(f"""
         <div style="display:flex; align-items:center; justify-content:center; gap:12px; margin-top:30px;">
             <img src='{logo_url}' style='height:85px; filter: drop-shadow(0 2px 8px rgba(0,0,0,0.1));' />
-            <h2 style='text-align:center; color:#4f46e5 !important; margin:0;'>SNAP<br>CLASS</h2>
+            <h2 style='text-align:center; color:#4f46e5 !important; margin:0;'>CLASS<br>TRACK</h2>
         </div>
     """, unsafe_allow_html=True)    
